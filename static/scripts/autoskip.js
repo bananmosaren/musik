288d7837dc74479fb44dd02d47fed0796afc234c
Bananmosaren <fred.josefsson@gmail.com>
2026-07-21 17:42:59 +0200

    titles

--- static/scripts/autoskip.js
+++ static/scripts/autoskip.js
@@ -1,13 +1,23 @@
 const elements = document.getElementsByTagName("audio");
 
+const album = document.title;
 for (let i = 0; i < elements.length; i++) {
-    const current = elements.item(i);
-    const next = elements.item(i+1);
+    let current = elements.item(i);
+    let next = elements.item(i+1);
     
     current.addEventListener("ended", function()
         {
             current.currentTime = 0;
             next.play();
+
+            document.title = "–";
+        }
+    );
+    
+    current.addEventListener("play", function()
+        {
+            let title = current.title;
+            document.title = title + " – " + album;
         }
     );
 }
